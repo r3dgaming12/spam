18505025b7d6c5aba802c65591baba250a713397
r3dgaming12 <45930426+r3dgaming12@users.noreply.github.com>
2018-12-18 12:35:29 +0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -7,13 +7,13 @@ client.on("ready", () => {
 let channel =     client.channels.get("524093203197067285")
 setInterval(function() {
 channel.send(`R3D Spam bot ya m3lm hhhhhhhhhhh at7dak yl bot alb6l at7daaaaaaaaaak r3d hna #JOOOOOOOOOOOOOOOOOOOOOOOOO hhhhhhh i love r3d hhhh 
-channel.send(`R3D Spam bot ya m3lm hhhhhhhhhhh at7dak yl bot alb6l at7daaaaaaaaaak r3d hna #JOOOOOOOOOOOOOOOOOOOOOOOOO hhhhhhh i love r3d hhhh 
-channel.send(`R3D Spam bot ya m3lm hhhhhhhhhhh at7dak yl bot alb6l at7daaaaaaaaaak r3d hna #JOOOOOOOOOOOOOOOOOOOOOOOOO hhhhhhh i love r3d hhhh 
-channel.send(`R3D Spam bot ya m3lm hhhhhhhhhhh at7dak yl bot alb6l at7daaaaaaaaaak r3d hna #JOOOOOOOOOOOOOOOOOOOOOOOOO hhhhhhh i love r3d hhhh 
-channel.send(`R3D Spam bot ya m3lm hhhhhhhhhhh at7dak yl bot alb6l at7daaaaaaaaaak r3d hna #JOOOOOOOOOOOOOOOOOOOOOOOOO hhhhhhh i love r3d hhhh 
-channel.send(`R3D Spam bot ya m3lm hhhhhhhhhhh at7dak yl bot alb6l at7daaaaaaaaaak r3d hna #JOOOOOOOOOOOOOOOOOOOOOOOOO hhhhhhh i love r3d hhhh 
-channel.send(`R3D Spam bot ya m3lm hhhhhhhhhhh at7dak yl bot alb6l at7daaaaaaaaaak r3d hna #JOOOOOOOOOOOOOOOOOOOOOOOOO hhhhhhh i love r3d hhhh 
-channel.send(`R3D Spam bot ya m3lm hhhhhhhhhhh at7dak yl bot alb6l at7daaaaaaaaaak r3d hna #JOOOOOOOOOOOOOOOOOOOOOOOOO hhhhhhh i love r3d hhhh `);
+channel.send(` 
+channel.send(`
+channel.send(` 
+channel.send(` 
+channel.send(` 
+channel.send(` 
+channel.send(` `);
 }, 30)
 })
 
